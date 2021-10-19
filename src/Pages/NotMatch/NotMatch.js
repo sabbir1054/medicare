e821d8115ff1fe410431a7ffd075c936a3dca13f
Your Name <you@example.com>
2021-10-19 12:40:55 +0600

    404 page updated

--- src/Pages/NotMatch/NotMatch.js
+++ src/Pages/NotMatch/NotMatch.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import './NotMatch.css'
 const NotMatch = () => {
     return (
-        <div>
-            404
-        </div>
+      <div className="notMatch text-center">
+        <h1 className="display-1 error fw-bold   text-center">404</h1>
+        <p className="text-center fw-bold  small">page not found </p>
+        <NavLink to="/">
+          <button className="btn button py-2">Back to Home Page</button>
+        </NavLink>
+      </div>
     );
 };
 
